c717afe1da569af3b9a317d1a0cf0f163ceef6ee
Zabeehullah Qayumi <zabeeh.qayumi@gmail.com>
2020-11-04 19:55:22 -0800

    Added some new more lines

--- SampleBranching/ViewController.swift
+++ SampleBranching/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // addded some comments
+        // added more new lines
     }
 }
 
